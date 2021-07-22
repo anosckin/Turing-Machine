8f0dcb13dd6dc187845e5c1df61323b46591e9b6
Nika Salia <nsali19@freeuni.edu.ge>
2021-07-22 17:32:09 +0400

    simulator is done (still needs testing)

---
+++ convert.cpp
@@ -0,0 +1,18 @@
+#pragma GCC diagnostic warning "-std=c++11"
+#include<bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define f first
+#define s second
+#define INF INT_MAX
+#define MOD 1000000007
+#define pause system("pause")
+#define all(x) (x).begin(),(x).end()
+#define deb(x) cout << #x << " " << x << endl
+using namespace std;
+main () {
+	ios::sync_with_stdio(0);
+}
+
+
+
